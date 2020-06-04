b952d5713bc69a78024230dcb36f6626af5ed556
Baily Case <baily@hailtrace.com>
2020-06-04 18:45:45 -0500

    fix: fixed server to https

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from "apollo-server-express";
+import fs from 'fs'
 import "reflect-metadata";
-import http from "http";
+import https from "https";
 import { mergeTypes, mergeResolvers } from "merge-graphql-schemas";
 import { cardsModule } from "./src/cards/index";
 import { userModule } from "./src/users/index";
@@ -20,6 +21,7 @@ const resolvers = mergeResolvers([
   userModule.resolvers,
   gameModule.resolvers,
 ]);
+
 
 const options = {
   host: "10.15.246.162",
@@ -44,7 +46,13 @@ const app = express();
 
 server.applyMiddleware({ app });
 
-const httpServer = http.createServer(app);
+const httpServer = https.createServer(
+  {
+    key: fs.readFileSync(`./ssl/caf.key`),
+    cert: fs.readFileSync(`./ssl/caf.crt`)
+  },
+  app
+)
 server.installSubscriptionHandlers(httpServer);
 httpServer.listen(port, () => {
   buildView();
